eaec99c0f8f57c1669432a0bdbd673c2f8e8372d
Daniel Heppner <heppned@wwu.edu>
2019-03-20 14:44:20 -0700

    everything is working as expected!

--- amplify/backend/function/accountattributesCRUD/src/app.js
+++ amplify/backend/function/accountattributesCRUD/src/app.js
@@ -19,7 +19,7 @@ if(process.env.ENV && process.env.ENV !== "NONE") {
   tableName = tableName + '-' + process.env.ENV;
 }
 
-const userIdPresent = false; // TODO: update in case is required to use that definition
+const userIdPresent = true; // TODO: update in case is required to use that definition
 const partitionKeyName = "userId";
 const partitionKeyType = "S";
 const sortKeyName = "";
---
+++ notes.md
@@ -0,0 +1,4 @@
+## dynamodb user attributes
+- set "userIdPresent" const in app.js to true
+## cognito
+- add the custom 'apartmentPrefsSet' to user pool attributes
